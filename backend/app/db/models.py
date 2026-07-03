5b96a09b2478226f2440fdffecd5d682cef595d0
Timothy Shin <tmshin413@gmail.com>
2026-07-03 19:11:02 -0400

    slightly modified models.py

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -4,14 +4,13 @@ from typing import Optional
 
 class Base(DeclarativeBase):
     pass
-
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
-
+    
 class Note(Base):
     __tablename__ = "notes"
 
