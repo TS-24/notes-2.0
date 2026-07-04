94595a5c12ea7e1a282f5f3cc46107febeecca5e
Timothy Shin <tmshin413@gmail.com>
2026-07-04 13:04:41 -0400

    Adde backend dockerfile

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,15 +1,24 @@
-from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy import Table, Column, String, Text, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from typing import Optional
+from typing import Optional, List
 
 class Base(DeclarativeBase):
     pass
+
+note_word_association = Table(
+    "note_word",
+    Base.metadata,
+    Column("note_id", ForeignKey("notes.id"), primary_key=True),
+    Column("word_id", ForeignKey("word_definitions.id"), primary_key=True),
+)
+
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    note: Mapped[List["Note"]] = relationship(back_populates="author")
     
 class Note(Base):
     __tablename__ = "notes"
@@ -18,6 +27,12 @@ class Note(Base):
     title: Mapped[str] = mapped_column(String(255), nullable=False)
     content: Mapped[Optional[str]] = mapped_column(Text)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    author: Mapped["User"] = relationship(back_populates="notes")
+
+    words : Mapped[List["WordDefinition"]] = relationship(
+        secondary=note_word_association,
+        back_populates="notes"
+        )
 
 class WordDefinition(Base):
     __tablename__ = "word_definitions"
@@ -25,5 +40,8 @@ class WordDefinition(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     word: Mapped[str] = mapped_column(String(255), nullable=False)
     definition: Mapped[Optional[str]] = mapped_column(Text)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    note_id: Mapped[Optional[int]] = mapped_column(ForeignKey("notes.id"))
+
+    notes: Mapped[List["Note"]] = relationship(
+        secondary=note_word_association, 
+        back_populates="words"
+    )
